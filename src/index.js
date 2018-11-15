c83ba11a4fba6e82f47ec92c06362e5777646aa1
EyePiercings <doshawareb@gmail.com>
2018-11-15 14:47:29 -0500

    First commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,127 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Box extends Component {
+  selectBox = () => {
+    this.props.selectBox(this.props.row, this.props.col)
+  }
+  // Note: no way to pass to this.props.etc if its inside the render method.
+  // That's why we make selectBox its own method to pass props into it
+  render() {
+    return (
+      <div 
+        className={this.props.boxClass}
+        id={this.props.id}
+        onClick={this.selectBox}
+      />
+    );
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Grid extends Component {
+  render() {
+    const width = (this.props.cols * 16);
+    var rowsArr = [];
+    var boxClass = "";
+
+    for(var i = 0; i < this.props.rows;i++) {
+      for(var j = 0; j < this.props.cols;j++) {
+        let boxId = i + "_" + j;
+
+        boxClass = this.props.gridFull[i][j] ? "box on" : "box off";
+        rowsArr.push(
+          <Box 
+            boxClass={boxClass}
+            key={boxId}
+            boxId={boxId}
+            row={i}
+            col={j}
+            selectBox={this.props.selectBox}
+          />
+        );
+      }
+    }
+
+    return (
+      <div className="grid" style={{width: width}}>
+        {rowsArr}
+      </div>
+    );
+  }
+}
+
+class Main extends Component {
+  constructor() {
+    super();
+    this.speed = 100;
+    this.rows = 30;
+    this.cols = 50;
+
+    this.state = {
+      generation: 0,
+      gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false))
+    };
+  }
+
+  selectBox = (row, col) => {
+    let gridCopy = arrayClone(this.state.gridFull);
+    gridCopy[row][col] = !gridCopy[row][col];
+    this.setState({
+      gridFull : gridCopy
+    })
+  }
+
+  seed = () => {
+    let gridCopy = arrayClone(this.state.gridFull);
+    for(let i = 0; i < this.rows;i++) {
+      for(let j = 0; j < this.cols;j++) {
+        if (Math.floor(Math.random() * 4) === 1) {
+          gridCopy[i][j] = true;
+        }
+      }
+    }
+    this.setState({
+      gridFull : gridCopy
+    })
+  }
+
+  playButton = () => {
+    clearInterval(this.intervalId);
+    this.intervalId = setInterval(this.play, this.speed);
+  }
+
+  play = () => {
+    let g = this.state.gridFull;
+    let g2 = arrayClone(this.state.gridFull);
+
+    
+  }
+
+  componentDidMount() {
+    this.seed();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>The Game of Life</h1>
+        <Grid 
+          gridFull={this.state.gridFull}
+          rows={this.rows}
+          cols={this.cols}
+          selectBox={this.selectBox}
+        />
+        <h2>Generations : {this.state.generation}</h2>
+      </div>
+    );
+  }
+}
+
+// Since we're dealing with a two-dimensional array, we have to deep clone it
+// instead of using slice()
+function arrayClone(arr) {
+  return JSON.parse(JSON.stringify(arr));
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
